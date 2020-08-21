ca74fc06cd37f4e85ca0c9bb1eb6057e72dcd149
kfalencik <kfalencik@gmail.com>
2020-08-21 17:19:17 +0100

    Add schema data

--- pages/index.vue
+++ pages/index.vue
@@ -31,6 +31,37 @@ export default {
     Contact,
     Social,
     Footer
-  }
+  },
+
+  data() {
+    return {
+      structuredData: {
+        "@context": "http://schema.org",
+        "@type" : "Organization",
+        "name" : "Inkdependent",
+        "url" : "http://inkdependent.eu",
+        "sameAs" : [
+          "https://www.facebook.com/inkdependenttattoos",
+          "https://www.instagram.com/inkdependenttattoos",
+        ],
+        "address": {
+          "@type": "PostalAddress",
+          "addressLocality": "Edinburgh",
+          "addressRegion": "Midlothian",
+          "postalCode": "EH11 2BA",
+          "streetAddress": "56 Dalry Road"
+        },
+        "telephone": "0131 623 65 65",
+        "email": "mailto:inkdependenttattoos@gmail.com",
+      }
+    }
+  },
+
+  head() {
+    return {
+      __dangerouslyDisableSanitizers: ['script'],
+      script: [{ innerHTML: JSON.stringify(this.structuredData), type: 'application/ld+json' }]
+    }
+  },
 }
 </script>
