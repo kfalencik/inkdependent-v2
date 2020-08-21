2dbd2043dc7216d10269e76a88bec761ec21ea9d
kfalencik <kfalencik@gmail.com>
2020-08-21 12:46:06 +0100

    Social

---
+++ components/Social.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="social" class="social u-white" v-cloak>
+    <div class="container">
+      <div class="row">
+        <div class="col u-text-center">
+          <h2 class="h2 u-cinzel">Stay up to date</h2>
+          <p class="u-cinzel">Don't forget to add us on social media to see latest artwork and infromation about guestspot artists</p>
+          <p>
+            <a rel="noopener" href="https://www.facebook.com/inkdependenttattoos" target="_blank" title="Facebook" class="social__link"><i class="fab fa-facebook-square"></i> inkdependenttattoos</a>
+            <a rel="noopener" href="https://www.instagram.com/inkdependenttattoos" target="_blank" title="Instagram" class="social__link"><i class="fab fa-instagram"></i> inkdependenttattoos</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
