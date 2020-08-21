cb8b8e19e143c754bf3c6ee23bee7527cfb5d03d
kfalencik <kfalencik@gmail.com>
2020-08-21 12:42:33 +0100

    Initial components

---
+++ pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="about u-text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+            <h1 class="h2">Sorry this page does not exist</h1>
+            <p class="u-cinzel">Please make sure you're trying to access an exisitng page and try again</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      Start
-    </div>
+  <div>
+    <Hero />
+    <Nav />
+    <About />
+    <Tattoo />
+    <Map />
+    <Contact />
+    <Social />
+    <Footer />
   </div>
 </template>
+
+<script>
+
+import Hero from '@/components/Hero'
+import Nav from '@/components/Nav'
+import About from '@/components/About'
+import Tattoo from '@/components/Tattoo'
+import Map from '@/components/Map'
+import Contact from '@/components/Contact'
+import Social from '@/components/Social'
+import Footer from '@/components/Footer'
+
+
+export default {
+  components: {
+    Hero,
+    Nav,
+    About,
+    Tattoo,
+    Map,
+    Contact,
+    Social,
+    Footer
+  }
+}
+</script>
