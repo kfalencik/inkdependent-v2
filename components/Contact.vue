343dd8e4a6976bfe97a580897f457c629d73cf09
kfalencik <kfalencik@gmail.com>
2020-08-27 15:24:02 +0100

    HOTFIX

--- components/Contact.vue
+++ components/Contact.vue
@@ -3,12 +3,12 @@
     <div class="container">
       <div class="row">
         <div class="col u-white u-text-center">
-          <h2 class="h2 u-cinzel" v-scroll-reveal="{ cleanup: true, delay: 500, duration: 1000, distance: '250px', origin: 'top', interval: 300 }">
+          <h2 class="h2 u-cinzel" v-scroll-reveal="{ delay: 500, duration: 1000, distance: '250px', origin: 'top', interval: 300 }">
             <span class="u-gold">Have any questions?</span> <span class="u-white">Let us know!</span>
           </h2>
-          <p class="contact__item" v-scroll-reveal="{ cleanup: true, delay: 500, duration: 1000, distance: '250px', origin: 'left', interval: 300 }"><fa :icon="['fas', 'map-marker-alt']"/> 56 DALRY ROAD, EH11 2BA, EDINBURGH, UK</p>
-          <p class="contact__item" v-scroll-reveal="{ cleanup: true, delay: 700, duration: 1000, distance: '250px', origin: 'left', interval: 300 }"><fa :icon="['fas', 'envelope']"/> <a href="mailto:INKDEPENDENTTATTOOS@GMAIL.COM">INKDEPENDENTTATTOOS@GMAIL.COM</a></p>
-          <p class="contact__item" v-scroll-reveal="{ cleanup: true, delay: 900, duration: 1000, distance: '250px', origin: 'left', interval: 300 }"><fa :icon="['fas', 'phone']"/> 0131 623 65 65</p>
+          <p class="contact__item" v-scroll-reveal="{ delay: 500, duration: 1000, distance: '250px', origin: 'left', interval: 300 }"><fa :icon="['fas', 'map-marker-alt']"/> 56 DALRY ROAD, EH11 2BA, EDINBURGH, UK</p>
+          <p class="contact__item" v-scroll-reveal="{ delay: 700, duration: 1000, distance: '250px', origin: 'left', interval: 300 }"><fa :icon="['fas', 'envelope']"/> <a href="mailto:INKDEPENDENTTATTOOS@GMAIL.COM">INKDEPENDENTTATTOOS@GMAIL.COM</a></p>
+          <p class="contact__item" v-scroll-reveal="{ delay: 900, duration: 1000, distance: '250px', origin: 'left', interval: 300 }"><fa :icon="['fas', 'phone']"/> 0131 623 65 65</p>
 
           <form id="contact-form" @submit="sendEmail" method="POST">
             <div class="container">
@@ -22,28 +22,28 @@
               <div class="row">
                 <div class="col-lg-4">
                   <label for="contact-name"><span class="sr-only">Name</span>
-                    <input id="contact-name" v-scroll-reveal="{ cleanup: true, delay: 1000, duration: 1000, distance: '250px', origin: 'top', interval: 300 }" v-model="contactName" name="from_name" placeholder="Your name" required />
+                    <input id="contact-name" v-scroll-reveal="{ delay: 1000, duration: 1000, distance: '250px', origin: 'top', interval: 300 }" v-model="contactName" name="from_name" placeholder="Your name" required />
                   </label>
                 </div>
 
                 <div class="col-lg-4">
                   <label for="contact-email"><span class="sr-only">Email</span>
-                    <input id="contact-email" v-scroll-reveal="{ cleanup: true, delay: 1200, duration: 1000, distance: '250px', origin: 'top', interval: 300 }" v-model="contactEmail" name="reply_to" type="email" placeholder="Your email" required />
+                    <input id="contact-email" v-scroll-reveal="{ delay: 1200, duration: 1000, distance: '250px', origin: 'top', interval: 300 }" v-model="contactEmail" name="reply_to" type="email" placeholder="Your email" required />
                   </label>
                 </div>
 
                 <div class="col-lg-4">
                   <label for="contact-subject"><span class="sr-only">Subject</span>
-                    <input id="contact-subject" v-scroll-reveal="{ cleanup: true, delay: 1400, duration: 1000, distance: '250px', origin: 'top', interval: 300 }" v-model="contactSubject" name="subject" placeholder="Your subject" required />
+                    <input id="contact-subject" v-scroll-reveal="{ delay: 1400, duration: 1000, distance: '250px', origin: 'top', interval: 300 }" v-model="contactSubject" name="subject" placeholder="Your subject" required />
                   </label>
                 </div>
 
                 <div class="col">
                   <label for="contact-message"><span class="sr-only">Message</span>
-                    <textarea id="contact-message" v-scroll-reveal="{ cleanup: true, delay: 1600, duration: 1000, distance: '250px', origin: 'top', interval: 300 }" v-model="contactMessage" name="message_html" placeholder="Your message" required></textarea>
+                    <textarea id="contact-message" v-scroll-reveal="{ delay: 1600, duration: 1000, distance: '250px', origin: 'top', interval: 300 }" v-model="contactMessage" name="message_html" placeholder="Your message" required></textarea>
                   </label>
 
-                  <button type="submit" class="btn" v-scroll-reveal="{ cleanup: true, delay: 1800, duration: 1000, distance: '250px', origin: 'right', interval: 300 }">Send your message</button>
+                  <button type="submit" class="btn" v-scroll-reveal="{ delay: 1800, duration: 1000, distance: '250px', origin: 'right', interval: 300 }">Send your message</button>
                 </div>
               </div>
             </div>
@@ -85,7 +85,7 @@ export default {
           message: this.contactMessage
         };
         
-        emailjs.send(service_id, template_id, message).then(response => {
+        emailjs.send(service_id, template_id, message, 'user_zMe7fkxzmtQpKn9E5lzTw').then(response => {
           this.contactMessage = '';
           this.contactName = '';
           this.contactEmail = '';
